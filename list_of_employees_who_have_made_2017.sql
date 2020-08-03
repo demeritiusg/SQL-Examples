4c3b4e1fe3cccd3955f929e1bcf4a38d67e2b58e
Demeritius Griffin <40575093+demeritiusg@users.noreply.github.com>
2020-08-03 13:59:04 -0500

    Create list_of_employees_who_have_made_2017.sql
    Initial

---
+++ list_of_employees_who_have_made_2017.sql
@@ -0,0 +1,15 @@
+SELECT
+	emp_name,
+	emp_ID
+FROM
+	emp_table
+WHERE
+	emp_ID IN 
+		(SELECT
+			emp_ID
+		FROM
+			emp_table
+		WHERE
+			emp_salary < 250000
+		AND
+			EXTRACT('year', effective_date) = '2017'
